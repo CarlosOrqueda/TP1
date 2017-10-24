3addf7e5003bf7a4747a969273f438975c8a8f70
CristianCod <cristian.alarcon.tec@gmail.com>
2017-10-24 11:48:13 -0300

    Comentarios

--- Trabajo_algoritmos_programacion.py
+++ Trabajo_algoritmos_programacion.py
@@ -10,13 +10,11 @@ import os
 
 # Menú
 # Declaro variable de inicio
-
 salir = False
 while salir == False or menu == 2:
     # si menu=2-vuelve al menú principal
     # si menu=1-vuelve al menu de letras o numerico segun corresponda
     opcion = funciones.menu()
-
     if opcion == 1:
         menu = 1
         while menu == 1:
@@ -24,14 +22,16 @@ while salir == False or menu == 2:
             opcion = funciones.sub_menu_num()
             # Selecciona opcion del menu numerico
             if opcion == 1:
+                #Solicita valores a calcular con msj por función.
                 mensaje_1 = str("Ingrese un numero: ")
                 mensaje_2 = str("Ingrese un numero: ")
                 num_1, num_2 = funciones.solicitud(mensaje_1, mensaje_2)
                 producto = funciones.multiplicacion(num_1, num_2)
                 funciones.imprimir(producto)
-                # print("pido valores")
+                #Pide nuevamente la opcion continuar
                 menu = funciones.opcion_submenu()
             elif opcion == 2:
+                #Solicita valores a calcular con msj por función.
                 mensaje_1 = str("Ingrese un dividendo: ")
                 mensaje_2 = str("Ingrese un divisor (No se puede dividir por cero): ")
                 dividendo, divisor = funciones.solicitud(mensaje_1, mensaje_2)
@@ -42,9 +42,10 @@ while salir == False or menu == 2:
                     mensaje = funciones.division(dividendo, divisor)
                     print(mensaje)
                     menu = funciones.opcion_submenu()
-                # print("pido valores")
+                #Pide nuevamente la opcion continuar
                 menu = funciones.opcion_submenu()
             elif opcion == 3:
+                #Solicita valores a calcular con msj por función.
                 mensaje_1 = str("Ingrese una base: ")
                 mensaje_2 = str("Ingrese un exponente: ")
                 base, exponente = funciones.solicitud(mensaje_1, mensaje_2)
@@ -54,11 +55,9 @@ while salir == False or menu == 2:
                 except:
                     mensaje = funciones.potencia(base, exponente)
                     print(mensaje)
-                    # menu=funciones.opcion_submenu()
-                # print("pido valores")
+                #Pide nuevamente la opcion continuar
                 menu = funciones.opcion_submenu()
             elif opcion == 4:
-                # opcion=1
                 menu = 0
                 # salir=0
                 # continuar=1
@@ -74,32 +73,29 @@ while salir == False or menu == 2:
                 cadena = funciones.armar_cadena(cadena)
                 cadena = funciones.palabra_mas_larga(cadena)
                 funciones.imprimir(cadena)
-                # print("pido valores")
                 menu = funciones.opcion_submenu()
             elif opcion == 2:
                 cadena = funciones.solicitar_cadena()
                 cadena = funciones.armar_cadena(cadena)
                 cadena = funciones.string_corto(cadena)
                 funciones.imprimir(cadena)
-                # print("pido valores")
                 menu = funciones.opcion_submenu()
             elif opcion == 3:
                 cadena = funciones.solicitar_cadena()
                 cadena = funciones.armar_cadena(cadena)
                 cadena = funciones.contar_cadena(cadena)
                 funciones.imprimir(cadena)
-                # print("pido valores")
                 menu = funciones.opcion_submenu()
             elif opcion == 4:
                 menu = 0
                 # salir=0
 
     elif opcion == 3:
-        # Sale de el programa
+        # Sale del programa
         print("Saliendo...")
         input("Presione una tecla para continuar...")
         salir = True
         menu = 0
 
     else:
-        print("No deberia llegar nunca acá")
+        print("No deberia llegar nunca acá(Las alternativas estan controladas en las excepciones del menú)")
